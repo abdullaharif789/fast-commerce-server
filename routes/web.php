2759568cc19c8e7fb7a4a94b380df9c910f96cf8
Abdullah Arif <96057151+abdullah-arif-qbatch@users.noreply.github.com>
2022-04-14 00:43:18 +0500

    update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Customer;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,3 +24,16 @@ Route::apiResource('users', 'App\Http\Controllers\UserController');
 Route::apiResource('customers', 'App\Http\Controllers\CustomerController');
 Route::apiResource('registrations', 'App\Http\Controllers\RegistrationController');
 Route::apiResource('payments', 'App\Http\Controllers\PaymentController');
+Route::get('/payment_email', function(){
+    $from = "admin@fcportal.com";
+    $to = "abdullaharif789@gmail.com";
+    $subject = "Payment Trigger";
+    $message = "Test Name : ".Customer::find(1)->name;
+    $headers = "From:" . $from;
+    if(mail($to,$subject,$message, $headers)) {
+        echo "The email message was sent.";
+    } else {
+        echo "The email message was not sent.";
+    }
+
+});
